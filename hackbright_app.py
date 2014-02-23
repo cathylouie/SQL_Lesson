49315a850b5cf0869b127640a34b558df84637f9
Hackbright Student <info@hackbrightacademy.com>
2014-02-23 15:03:57 -0800

    Cathy's attempt1 23Feb

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,6 +3,15 @@ import sqlite3
 DB = None
 CONN = None
 
+def get_project_by_title(title):
+    query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Title: %s 
+Description: %s
+Maximum grade: %s"""%(row[0], row[1], row[2])   
+
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
@@ -29,6 +38,19 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "project":
+            get_project_by_title(*args)
+        elif command == "new_project":
+            add_new_project(*args)
+
+def add_new_project(title, description, max_grade):
+    query = """INSERT into Projects values (PRIMARY KEY AUTOINCREMENT,?,?,?)"""
+    DB.execute(query, (title, description, max_grade))
+
+    CONN.commit()
+    print "Successfully added project:%s %s %s"%(title, description, max_grade)
+
+    CONN.close()          
 
 def make_new_student(first_name, last_name, github):
     query = """INSERT into Students values (?,?,?)"""
