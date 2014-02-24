a04096db84346f3b042adc820e7582787843fcc3
Hackbright Student <info@hackbrightacademy.com>
2014-02-23 18:08:29 -0800

    Cathy's attempt2 23Feb

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,6 +3,21 @@ import sqlite3
 DB = None
 CONN = None
 
+def get_grade_by_title(project_title):
+
+# Do some joining to make a VIEW called GradesView so to connect student's grade with student's name via student's github name,
+# then SELECT Students.first_name and students.last_name, Grades.grade WHERE Grades.project_title = raw_input.
+    #query = """ SELECT Students.first_name, Students.last_name, Grades.project_title, Grades.grade FROM Students INNER JOIN Grades ON (Students.github=Grades.student_github)"""
+
+
+    
+    DB.execute(query, (Grades.project_title,))
+    row = DB.fetchone()
+    print """\
+Title: %s 
+Student: %s %s
+Grade: %s"""%(row[0], row[1], row[2], row[4])
+
 def get_project_by_title(title):
     query = """SELECT title, description, max_grade FROM Projects WHERE title = ?"""
     DB.execute(query, (title,))
@@ -40,17 +55,17 @@ def main():
             make_new_student(*args)
         elif command == "project":
             get_project_by_title(*args)
-        elif command == "new_project":
-            add_new_project(*args)
+    #    elif command == "new_project":
+    #        add_new_project(*args)
 
-def add_new_project(title, description, max_grade):
-    query = """INSERT into Projects values (PRIMARY KEY AUTOINCREMENT,?,?,?)"""
-    DB.execute(query, (title, description, max_grade))
+#def add_new_project(title, description, max_grade): """Couldn't figure out how to INSERT the autoincrement id and a sentence for the'description' field
+#    query = """INSERT into Projects values (?,?,?)"""
+#    DB.execute(query, (title, description, max_grade))
 
-    CONN.commit()
-    print "Successfully added project:%s %s %s"%(title, description, max_grade)
+#    CONN.commit()
+#   print "Successfully added project:%s %s %s"%(title, description, max_grade)
 
-    CONN.close()          
+#    CONN.close()          
 
 def make_new_student(first_name, last_name, github):
     query = """INSERT into Students values (?,?,?)"""
