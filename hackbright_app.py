4411013f0c44ffd51b699456b7587adff50a552f
Hackbright Student <info@hackbrightacademy.com>
2014-02-28 18:53:45 -0800

    Feb 28 Cathy's Pt3 attempt

--- hackbright_app.py
+++ hackbright_app.py
@@ -18,6 +18,15 @@ def get_project_title(id):
     except:
         return None
 
+def get_all_students(id):
+    query = """SELECT title FROM Projects WHERE id = ?"""
+    DB.execute(query,(id))
+    try:
+        row = DB.fetchone()
+        return row[0]
+    except:
+        return None
+
 def get_grade_by_title(project_title):
     query = """SELECT first_name, last_name, student_github, project_title, grade FROM Grades JOIN Students ON github=student_github WHERE project_title = ?""" 
     DB.execute(query, (project_title,))
@@ -34,7 +43,7 @@ def get_project_by_title(title):
     row = DB.fetchone()
     print """\
     Title: %s 
-    Description: %s
+    Description: %s2
     Maximum grade: %s"""%(row[0], row[1], row[2])   
 
 def get_student_by_github(github):
@@ -54,10 +63,11 @@ def get_grade_by_github(grade):
     # Grade: %s"""%(index[0], index[1], index[2], index[3])
 
 def make_new_student(first_name, last_name, github):
+    print first_name, last_name, github
     query = """INSERT into Students values (?,?,?)"""
     DB.execute(query, (first_name,last_name,github))
     CONN.commit()
-    print "Successfully added student: %s %s"%(first_name,last_name)
+    return "Successfully added student: %s %s %s"%(first_name,last_name, github)
 
 def add_new_project(title, description, max_grade): 
     query = """INSERT into Projects (title, description, max_grade) values (?,?,?)"""
@@ -69,7 +79,13 @@ def add_a_grade(student_github, project_title, grade):
     query = """INSERT into Grades (student_github, project_title, grade) values (?,?,?)"""
     DB.execute(query, (student_github, project_title, grade))
     CONN.commit()
-    print "Successfully added grade:%s %s %s"%(student_github, project_title, grade) 
+    return "Successfully added grade:%s %s %s"%(student_github, project_title, grade) 
+
+def change_a_grade(title, grade): 
+    query = """UPDATE Grades SET grade = ? WHERE project_title = ?"""
+    DB.execute(query, (title, grade))
+    CONN.commit()
+    return "Successfully changed grade:%s"%(grade)
 
 def connect_to_db():
     global DB, CONN
--- webapp.py
+++ webapp.py
@@ -13,16 +13,29 @@ def get_project_title():
 def get_github():
     return render_template("get_github.html")
 
+@app.route("/new_student")
+def make_new_student():
+    return render_template("make_new_student.html")
+
+@app.route("/new_project")
+def add_new_project():
+    return render_template("add_new_project.html")
+
+@app.route("/new_grade")
+def add_new_grade():
+    hackbright_app.connect_to_db()
+    projects = hackbright_app.get_all_projects()
+    return render_template("all_projects_for_grade.html", projects=projects)
+
 @app.route("/project/<id>/")
 def get_student_and_grade_from_project(id):
     hackbright_app.connect_to_db()
     title = hackbright_app.get_project_title(id)
     if title is None:
         abort(404)
-    print title
+    #print title
     list_of_grades = hackbright_app.get_grade_by_title(title)
     html = render_template("project_info.html", list_of_grades=list_of_grades)
-    print list_of_grades
     return html
 
 
@@ -36,7 +49,55 @@ def get_student():
                                                 last_name=row[1],
                                                 github=row[2],
                                                 list_of_grades=list_of_grades)
+    return html
 
+@app.route("/new_student/info/")
+def new_student_info():
+    hackbright_app.connect_to_db()
+    new_student_first_name = request.args.get("first_name")
+    new_student_last_name = request.args.get("last_name")
+    new_student_github = request.args.get("github")
+    hackbright_app.make_new_student(new_student_first_name, new_student_last_name, new_student_github)
+    html = render_template("completed_new_student.html",first_name=new_student_first_name,
+                                                        last_name=new_student_last_name,
+                                                        github=new_student_github)
+
+    return html
+
+@app.route("/new_project/info/")
+def new_project_info():
+    hackbright_app.connect_to_db()
+    new_project_title = request.args.get("title")
+    new_project_description = request.args.get("description")
+    new_project_max_grade = request.args.get("max_grade")
+    hackbright_app.add_new_project(new_project_title, new_project_description, new_project_max_grade)
+    html = render_template("completed_new_project.html",title=new_project_title,
+                                                        description=new_project_description,
+                                                        max_grade=new_project_max_grade)
+
+    return html
+
+@app.route("/new_grade/info/<id>")
+def new_grade_info(id):
+    hackbright_app.connect_to_db()
+    title = hackbright_app.get_project_title(id)
+    list_of_grades = hackbright_app.get_grade_by_title(title)
+    html = render_template("grade_input.html", list_of_grades=list_of_grades,
+                                               id=id)
+
+    return html
+
+@app.route("/completed_new_grade/info/<id>")
+def completed_new_grade(id):
+    hackbright_app.connect_to_db()
+    new_grade = request.args.get("grade")
+    title = hackbright_app.get_project_title(id)
+    list_of_grades = hackbright_app.get_grade_by_title(title)
+    row = hackbright_app.change_a_grade(title,new_grade)
+    print row
+    html = render_template("completed_new_grade.html", github=list_of_grades[0][2],
+                                                       project_title=title,
+                                                       grade=new_grade)
     return html
 
 if __name__ == "__main__":
